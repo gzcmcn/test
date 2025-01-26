c292077c9442d5f852f76b547d66485a0ff277b1
gzcmcn <guzhucanmeng@163.com>
2025-01-26 17:06:02 +0800

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,11 @@
+var voteList = [
+    {id:1, name:"张旭阳", college:"黑客聚集地的意淫一哥，天天管理备案的小破站，逐日计划阻挠者，疑似通日", vote:5, image:"../images/mmexport1737870415460.jpg"},
+    {id:2, name:"轩白", college:"精神病院跑出来的白眼狼，患有严重臆想症，黑客聚集地的意淫二哥，喜欢口嗨，以国五安名义肆意对原黑客组织俯瞰者抢人，导致俯瞰者解散", vote:5, image:"../images/mmexport1737870417050.jpg"},
+    {id:3, name:"苏鹤", college:"苏鹤：原昵称勾八，自己也是个勾八，与knight、轩白同流合污，黑客聚集地的大乐子，管理那老不死的破狙击者联盟网站", vote:20, image:"../images/mmexport1737870670745.jpg"},
+    {id:4, name:"地虎", college:"吃里扒外，名副其实的黑客聚集地意淫三哥，喜欢用各种小号伪装自己，说话阴阳怪气，曾辅助knight、轩白等人试图阻止逐日计划", vote:5, image:"../images/mmexport1737870418493.jpg"},
+    {id:5, name:"赵晓燕", college:"游艇学院", vote:1, image:"../images/美女5.jpg"},
+    {id:6, name:"钟小婷", college:"信通学院", vote:8, image:"../images/美女6.jpg"},
+    {id:7, name:"张妍", college:"船海学院", vote:10, image:"../images/美女7.jpg"},
+    {id:8, name:"李文冬", college:"海运学院", vote:9, image:"../images/美女8.jpg"},
+    {id:9, name:"陈燕", college:"海运学院", vote:5, image:"../images/美女9.jpg"}
+]
